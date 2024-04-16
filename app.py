ec0d4e8cc964b772d72525f28f4463a4b56ceca3
omvaddi <147666463+omvaddi@users.noreply.github.com>
2024-04-16 15:16:17 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -3,37 +3,29 @@ import random
 
 app = Flask(__name__)
 
-def solve_knapsack(matrix):
-    best_items = []
-    max_weight = 30
-    best_ratio = -1
-    while max_weight > 0 and best_ratio != 0:
-        best_ratio = 0  # Reset best_ratio for each iteration
-        selected_item = None
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                weight, value = matrix[i][j]
-                ratio = value / weight
-                if ratio > best_ratio and weight <= max_weight:
-                    best_ratio = ratio
-                    selected_item = (i, j)
-        if selected_item:
-            best_items.append(selected_item)
-            max_weight -= matrix[selected_item[0]][selected_item[1]][0]
-            matrix[selected_item[0]][selected_item[1]][1] = -1  # Mark selected item as used
-    return best_items
+
+class Knapsack:
+    def __init__(self):
+        self.items = []
+        self.dimensions = 10
+
+
+
+    def create_data(self):
+        for i in range(self.dimensions):
+            row = []
+            for j in range(self.dimensions):
+                weight = random.randint(0, 20)
+                value = random.randint(0, 20)
+                row.append((weight, value))
+            self.items.append(row)
 
 @app.route('/')
 def index():
-    # Create a 10x10 matrix of random weights and values
-    matrix = [[(random.randint(1, 10), random.randint(1, 100)) for _ in range(10)] for _ in range(10)]
-    return render_template('index.html', matrix=matrix)
+    knapsack = Knapsack()
+    knapsack.create_data()
+    return render_template('index.html', knapsack=knapsack)
 
-@app.route('/solve_knapsack', methods=['POST'])
-def solve_knapsack_route():
-    matrix = request.json['matrix']
-    best_items = solve_knapsack(matrix)
-    return jsonify({'best_items': best_items})
 
 if __name__ == '__main__':
     app.run(debug=True)
