8d1dc2eb28ed87dcf0b726d3aad7cae9ff798e7a
omvaddi <147666463+omvaddi@users.noreply.github.com>
2024-04-20 15:12:27 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import random
+
 app = Flask(__name__)
+
+
 class Knapsack:
     def __init__(self):
         self.items = []
-        self.dimensions = 4
+        self.dimensions = 5
         self.greedy_sol = []
         self.create_data()
         self.weight = 30
@@ -22,9 +25,11 @@ class Knapsack:
 
     def greedy(self):
         ratio = []
+
         for i in range(self.dimensions):
             row = [False] * self.dimensions
             self.greedy_sol.append(row)
+
         index = 0
         # creates 1d array of ratios
         for i in range(self.dimensions):
@@ -32,13 +37,18 @@ class Knapsack:
                 # array contains index value and ratio
                 ratio.append((index, self.items[i][j][1]/self.items[i][j][0]))
                 index += 1
+
         # sorts by ratio
         ratio.sort(key=lambda x : x[1], reverse = True)
+
         avaliable_space = self.weight
+
         for i in range(len(ratio)):
+
             # get 2d equivalent of indexes
             row = ratio[i][0] // self.dimensions
             col = ratio[i][0] % self.dimensions
+
             if(avaliable_space - self.items[row][col][0] >= 0):
                 avaliable_space = avaliable_space - self.items[row][col][0]
                 self.greedy_sol[row][col] = True
@@ -49,12 +59,31 @@ class Knapsack:
 @app.route('/')
 def index():
     knapsack = Knapsack()
+    knapsack.greedy()
+
     for row in knapsack.items:
         print(row)
-    knapsack.greedy()
+
     for row in knapsack.greedy_sol:
         print(row)
+
     return render_template('index.html', knapsack=knapsack)
+
+@app.route('/recalculate', methods=['POST'])
+def recalculate():
+    knapsack = Knapsack()
+    knapsack.greedy()
+
+    for row in knapsack.items:
+        print(row)
+
+    for row in knapsack.greedy_sol:
+        print(row)
+
+    # package data for html
+    recalculated_data = {'items': knapsack.items, "greedy_sol": knapsack.greedy_sol}
+
+    return jsonify(recalculated_data)
 
 
 if __name__ == '__main__':
